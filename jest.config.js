0f8c0a6fc0b2f2a696f2ca1b12faf1f3d05b4217
Victor <victorgustafsson@hotmail.com>
2022-06-03 15:09:28 +0200

    make tests run

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   testEnvironment: "node",
   reporters: ["default"],
-  testMatch: ['**/*.spec.js'],
+  testMatch: ["**/*.spec.js"],
   haste: {
     enableSymlinks: true,
   },
+  collectCoverage: true,
   moduleNameMapper: {
-    'coverage_test/(.*)': '<rootDir>/$1',
+    "coverage_test/(.*)": "<rootDir>/$1",
   },
 };
